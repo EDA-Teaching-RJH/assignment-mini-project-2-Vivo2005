9d8ac6cd14934a3ebd40835188a7897cc5b38123
Vivo2005 <lvm1@kent.ac.uk>
2026-03-08 19:49:00 +0000

    opening text written

--- workspace1.py
+++ workspace1.py
@@ -1,5 +1,31 @@
-#regular expressions
+
+
+
+
+def main():
+    print("Welcome to the National Veterinary Registry")
+
+    x=1
+    print ("..Opening profile...")
+    while x<4:
+        print("...")
+        x=x+1
+    
+
+
 #testing
+
+
+# regex
+
+
 #libraries
+
+
 #input output
+
+
 #object oriented programming
+
+if __name__=="__main__":
+    main()
