f6b75d7a6658c570537d941c5f640d73ad897e2e
Vivo2005 <lvm1@kent.ac.uk>
2026-03-09 13:41:01 +0000

    trying to build my classes, cant figure out sys.argv but ill get it

--- workspace1.py
+++ workspace1.py
@@ -1,4 +1,6 @@
 import math
+import sys
+import cowsay #do properly when i can remember how
 
 def main_menu():
     print ("Please select from the following options:\n"
@@ -8,7 +10,36 @@ def main_menu():
     "4. change personal info\n"
     "5. change pet info")
 
+#building pet class with all basic info, species specific will branch off   
 
+class pet :
+    def __init__(self, name, owner, postcode ,age):
+        self.name=name
+        self.owner=owner
+        self.postcode=postcode
+        self.age=age
+    def __str__(self):
+        return f"{self.name}is owned by{self.owner},{self.postcode}"
+
+#sub class cat
+class cat(pet):
+    def __init__(self, name, owner, postcode ,age , breed):
+        super().__init__(name, owner, postcode, age)
+        self.breed=breed
+    def __str__(self):
+        return super().__str__()
+    
+
+#sub class dog 
+class dog(pet):
+    def __init__(self, name, owner, postcode ,age ,breed):
+        super().__init__(name, owner, postcode ,age)
+        self.breed=breed
+    def __str__(self):
+        return super().__str__()
+
+def log_new():
+    
 
 def main():
     print("Welcome to the National Veterinary Registry")
@@ -19,8 +50,10 @@ def main():
         print("...")
         x=x+1
 
+
+#tying to include sys.argv into the selection stage of my code .. failing
     main_menu()
-    n=int(input("Option:"))
+    n= sys.argv
 
     if n==1:
         print("hi")
